890a36d0551d715d6659bbc89e754bfffb1cc800
Ryan L. Bell <ryan.l.bell@gmail.com>
2012-12-29 22:20:20 -0500

    FizzBuzz... PRIME!

--- fizz-buzz.hs
+++ fizz-buzz.hs
@@ -7,3 +7,23 @@ fizzBuzz xs = [ fizzOrBuzz x | x <- xs ]
                                | n `mod`  3 == 0 = "Fizz"
                                | n `mod`  5 == 0 = "Buzz"
                                | otherwise = show n
+
+-- Sexy. No modulo or guards. Also, no parameters
+fizzBuzz' :: [String]
+fizzBuzz' = zipWith3 buzzer threes fives [1..]
+    where buzzer "" "" n = show n
+          buzzer s1 s2 _ = s1 ++ s2
+          threes = cycle ["", "", "Fizz"]
+          fives  = cycle ["", "", "", "", "Buzz"]
+
+-- Example usages of fizzBuzz'
+
+-- FizzBuzz one through fifteen
+-- putStrLn . unlines . take 15 $ fizzBuzz'
+
+-- FizzBuzz 26 - 50
+-- putStrLn . unlines . drop 25 . take 50 $ fizzBuzz'
+
+-- or take the 15 numbers after 30
+-- putStrLn . unlines . take 15 . drop 30 $ fizzBuzz' 
+
