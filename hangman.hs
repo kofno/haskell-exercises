188973a843410740ba35039c71f7aba346e13175
Ryan L. Bell <ryan.l.bell@gmail.com>
2013-05-31 09:00:10 -0400

    [Hangman] Hangman randomly selects solutions from a file.
    - Solutions can be phrases (have spaces), but still no punctuation.
    - capital letters are still treated differently
    - Still haven't made my own monad :cry:

--- hangman.hs
+++ hangman.hs
@@ -1,4 +1,5 @@
 import System.Console.ANSI
+import System.Random
 import Data.List
 
 type Solution = [Char]
@@ -21,39 +22,57 @@ guess game@(solution, guesses) c
   | otherwise      = (solution, c:guesses)
   where alreadyGuessed = c `elem` guesses
 
+missedGuesses :: Hangman -> Int
+missedGuesses (solution, guesses) = length [ x | x <- guesses
+                                               , x `notElem` solution ]
+
 check :: Hangman -> Result
-check (solution, guesses)
+check game@(solution, guesses)
   | loser         = Loss "Hanged!"
   | completedWord = Win "Phew! You won"
   | otherwise     = GameOn
-  where completedWord = solution == [ x | x <- solution, x `elem` guesses] 
-        loser         = length [ x | x <- guesses, x `notElem` solution] > 5
+  where completedWord = solution == [ x | x <- solution, x `elem` guesses || x == ' '] 
+        loser         = missedGuesses game > 5
 
 main :: IO ()
-main = gameLoop $ createGame "solution"
+main = do
+  solution <- readSolution
+  gameLoop $ createGame solution
+
+readSolution :: IO String
+readSolution = do
+  solutions  <- readFile "hangmanSolutions.txt"
+  i          <- randomRIO (0, length' solutions - 1)
+  return (lines solutions !! i)
+  where length' solutions = length . lines $ solutions
 
 gameLoop :: Hangman -> IO ()
 gameLoop game = do
+  putScreen game
+  letter <- getChar
+  case check $ newGame letter of Loss cs -> putEndGame cs
+                                 Win cs  -> putEndGame cs
+                                 GameOn  -> gameLoop $ newGame letter
+    where newGame c = guess game c
+
+putScreen :: Hangman -> IO ()
+putScreen game = do
   clearScreen
   putStrLn ""
   putStrLn $ maskSolution game
   putStrLn $ unusedLetters game
+  putStrLn $ "Number of misses: " ++ show (missedGuesses game)
   putStr "Enter a guess: "
-  letter <- getChar
-  case check $ newGame letter of Loss cs -> do
-                                   clearScreen
-                                   putStrLn ""
-                                   putStrLn cs
-                                 Win cs  -> do
-                                   clearScreen
-                                   putStrLn ""
-                                   putStrLn cs
-                                 GameOn  -> gameLoop $ newGame letter
-    where newGame c = guess game c
+
+putEndGame :: String -> IO ()
+putEndGame msg = do
+  clearScreen
+  putStrLn ""
+  putStrLn msg
 
 maskSolution :: Hangman -> String
 maskSolution (solution, guesses) = intersperse ' ' $ map (mask guesses) solution
-  where mask cs c = if c `elem` cs then c else '_'
+  where mask cs c = if c `elem` cs || c == ' ' then c else '_'
 
 unusedLetters :: Hangman -> String
 unusedLetters (_, guesses) = intersperse ' ' $ map (mask guesses) ['a'..'z']
